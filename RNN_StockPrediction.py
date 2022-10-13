398b91afdeac1938f8ee726ca205911c40d696c1
jjamongisbest <114421645+jjamongisbest@users.noreply.github.com>
2022-10-13 17:01:45 +0900

    Update RNN_StockPrediction.py

--- RNN_StockPrediction.py
+++ RNN_StockPrediction.py
@@ -1,6 +1,3 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 import numpy as np
@@ -11,17 +8,38 @@ mk = Analyzer.MarketDB()
 stocks = input('종목을 입력하세요 : ')
 raw_df = mk.get_daily_price(stocks, '2021-01-01', '2022-09-30')
 
-window_size = 10 
+window_size = 10
 data_size = 5
 
 def MinMaxScaler(data):
-    """최솟값과 최댓값을 이용하여 0 ~ 1 값으로 변환"""
     numerator = data - np.min(data, 0)
     denominator = np.max(data, 0) - np.min(data, 0)
-    # 0으로 나누기 에러가 발생하지 않도록 매우 작은 값(1e-7)을 더해서 나눔
-    return numerator / (denominator + 1e])
+    return numerator / (denominator + 1e-7)
+
+dfx = raw_df[['open','high','low','volume', 'close']]
+dfx = MinMaxScaler(dfx)
+dfy = dfx[['close']]
+
+x = dfx.values.tolist()
+y = dfy.values.tolist()
+
+data_x = []
+data_y = []
+window_size = 10
+for i in range(len(y) - window_size):
+    _x = x[i : i + window_size] # 다음 날 종가(i+windows_size)는 포함되지 않음
+    _y = y[i + window_size]     # 다음 날 종가
+    data_x.append(_x)
+    data_y.append(_y)
+
+print(_x, "->", _y)
+
+#훈련용 데이터셋
+train_size = int(len(data_y) * 0.7)
+train_x = np.array(data_x[0 : train_size])
 train_y = np.array(data_y[0 : train_size])
 
+#테스트용 데이터셋
 test_size = len(data_y) - train_size
 test_x = np.array(data_x[train_size : len(data_x)])
 test_y = np.array(data_y[train_size : len(data_y)])
@@ -51,10 +69,9 @@ plt.ylabel('stock price')
 plt.legend()
 plt.show()
 
-# raw_df.close[-1] : dfy.close[-1] = x : pred_y[-1]
-
+# 다음 날 주가의 상한/하한 예측하기
 today_SEC_price = pred_y[-2]
-tomorrow_SEC_price =pred_y[-1]
+tomorrow_SEC_price = pred_y[-1]
 
 if today_SEC_price < tomorrow_SEC_price:
     upper_price = (tomorrow_SEC_price - today_SEC_price) / today_SEC_price * 100
@@ -64,5 +81,3 @@ elif today_SEC_price > tomorrow_SEC_price:
     print("선택하신 종목은 다음 날",round(float(lower_price), 2), "% 하한가로 예상됩니다.")
 else:
     print("선택하신 종목은 다음 날 주식 동결입니다.")
-
-# 가격 예측에 어려움이 있어 추세를 파악하여 상한 및 하한을 예상해주기로 함
