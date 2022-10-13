7213aee0ed44714c4404bfadc1881658416a4a47
jjamongisbest <114421645+jjamongisbest@users.noreply.github.com>
2022-10-13 16:48:36 +0900

    Update RNN_StockPrediction.py

--- RNN_StockPrediction.py
+++ RNN_StockPrediction.py
@@ -1,56 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, LSTM, Dropout
-import numpy as np
-import matplotlib.pyplot as plt
-from Investar import Analyzer
-
-mk = Analyzer.MarketDB()
-stocks = input('종목을 입력하세요 : ')
-raw_df = mk.get_daily_price(stocks, '2021-01-01', '2022-09-30')
-
-window_size = 10 
-data_size = 5
-
-def MinMaxScaler(data):
-    """최솟값과 최댓값을 이용하여 0 ~ 1 값으로 변환"""
-    numerator = data - np.min(data, 0)
-    denominator = np.max(data, 0) - np.min(data, 0)
-    # 0으로 나누기 에러가 발생하지 않도록 매우 작은 값(1e-7)을 더해서 나눔
-    return numerator / (denominator + 1e-7)
-
-dfx = raw_df[['open','high','low','volume', 'close']]
-dfx = MinMaxScaler(dfx)
-dfy = dfx[['close']]
-
-x = dfx.values.tolist()
-y = dfy.values.tolist()
-
-data_x = []
-data_y = []
-for i in range(len(y) - window_size):
-    _x = x[i : i + window_size] # 다음 날 종가(i+windows_size)는 포함되지 않음
-    _y = y[i + window_size]     # 다음 날 종가
-    data_x.append(_x)
-    data_y.append(_y)
-
-print(_x, "->", _y)
-
-train_size = int(len(data_y) * 0.7)
-train_x = np.array(data_x[0 : train_size#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -106,16 +53,16 @@ plt.show()
 
 # raw_df.close[-1] : dfy.close[-1] = x : pred_y[-1]
 
-today_SEC_price = raw_df.close[-2] * pred_y[-2] / dfy.close[-1]
-tomorrow_SEC_price = raw_df.close[-1] * pred_y[-1] / dfy.close[-1]
+today_SEC_price = pred_y[-2]
+tomorrow_SEC_price =pred_y[-1]
 
 if today_SEC_price < tomorrow_SEC_price:
     upper_price = (tomorrow_SEC_price - today_SEC_price) / today_SEC_price * 100
-    print("선택하신 종목은 익일",round(float(upper_price), 2), "% 상한가로 예상됩니다.")
+    print("선택하신 종목은 다음 날",round(float(upper_price), 2), "% 상한가로 예상됩니다.")
 elif today_SEC_price > tomorrow_SEC_price:
     lower_price = (today_SEC_price - tomorrow_SEC_price) / today_SEC_price * 100
-    print("선택하신 종목은 익일",round(float(lower_price), 2), "% 하한가로 예상됩니다.")
+    print("선택하신 종목은 다음 날",round(float(lower_price), 2), "% 하한가로 예상됩니다.")
 else:
-    print("선택하신 종목은 익일 주식 동결입니다.")
+    print("선택하신 종목은 다음 날 주식 동결입니다.")
 
 # 가격 예측에 어려움이 있어 추세를 파악하여 상한 및 하한을 예상해주기로 함
